41f04585c07d48b62484cba725d0aeb998390ebd
John J. Franey <jjfraney@gmail.com>
2016-10-10 21:11:05 -0400

    adds command to a running 'main' for nominal testing.

--- src/main/java/org/jjfflyboy/mpc4j/MpdSample.java
+++ src/main/java/org/jjfflyboy/mpc4j/MpdSample.java
@@ -11,13 +11,19 @@ public class MpdSample {
 
     public static void main(String [] args) throws IOException {
 
+        run(new Ping());
+        run(new Update());
+        run(new SetVol(30));
+        run(new Crossfade(3));
         run(new Status());
+
         Status.Response r = mpc.send(new Status());
+
         System.out.println("volume=" + r.getVolume().orElse(-1));
         System.out.println("mix ramp db: " + r.getMixRampDb().orElse(null));
         System.out.println("error=" + r.getError().orElse("no error"));
 
-        mpc.idle(e -> { Stream.of(e).forEach(System.out::println); return true; });
+        //mpc.idle(e -> { Stream.of(e).forEach(System.out::println); return true; });
 
         //run(new Update());
     }
