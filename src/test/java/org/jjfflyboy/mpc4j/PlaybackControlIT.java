adabb6a47a78615dd271facef6e1d3f1bd1530f2
John J. Franey <jjfraney@gmail.com>
2016-10-19 21:16:14 -0400

    adds IT test for playback control

---
+++ src/test/java/org/jjfflyboy/mpc4j/PlaybackControlIT.java
@@ -0,0 +1,43 @@
+package org.jjfflyboy.mpc4j;
+
+import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.arquillian.junit.Arquillian;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.io.IOException;
+import java.lang.annotation.Repeatable;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Tests the playback control type commands
+ * @author jfraney
+ */
+@RunWith(Arquillian.class)
+public class PlaybackControlIT {
+    private MPC mpc;
+    @Before
+    public void before() {
+        mpc = new MPC();
+    }
+
+    private static int count =0;
+    @Test
+    @RunAsClient
+    public void testPlay() throws IOException {
+        Play playCommand = new Play();
+        Stop stopCommand = new Stop();
+
+        Play.Response playResponse = mpc.send(playCommand);
+        Stop.Response stopResponse = mpc.send(stopCommand);
+
+        assertThat(playResponse.isOk()).as("play starts").isTrue();
+        assertThat(stopResponse.isOk()).as("play stopped").isTrue();
+
+        Status.Response statusResponse = mpc.send(new Status());
+        assertThat(statusResponse.getState().get()).as("is not playing").isEqualTo("stop");
+    }
+
+}
