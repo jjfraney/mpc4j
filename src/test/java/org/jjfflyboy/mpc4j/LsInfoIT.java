9f51c887934d2302f1a50f2477d4ba9af1a28366
John J. Franey <jjfraney@gmail.com>
2016-10-21 19:39:04 -0400

    implements IT test for lsinfo command
    provides three songs for testing.

---
+++ src/test/java/org/jjfflyboy/mpc4j/LsInfoIT.java
@@ -0,0 +1,40 @@
+package org.jjfflyboy.mpc4j;
+
+import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.arquillian.junit.Arquillian;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.io.IOException;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Tests the playback control type commands
+ * @author jfraney
+ */
+@RunWith(Arquillian.class)
+public class LsInfoIT {
+    private MPC mpc;
+    @Before
+    public void before() {
+        mpc = new MPC();
+    }
+
+    @Test
+    @RunAsClient
+    public void test() throws IOException {
+        LsInfo.Response r = mpc.send(new LsInfo());
+
+        assertThat(r.isOk()).isTrue();
+
+        assertThat(r.getSongs().size()).isEqualTo(3);
+
+        List<Info.Response.Song> songs = r.getSongs();
+        Info.Response.Song song = songs.stream().filter(s -> s.getFile().get().equals("w1.ogg")).findAny().get();
+        assertThat(song.getLastModified().get()).isEqualTo("2016-10-21T21:06:03Z");
+    }
+
+}
