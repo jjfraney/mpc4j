a31b9d4a66d3343daa44277305286cc8da37028e
John J. Franey <jjfraney@gmail.com>
2016-10-26 23:08:08 -0400

    implements moveid command

---
+++ src/test/java/org/jjfflyboy/mpc4j/MoveIdTest.java
@@ -0,0 +1,17 @@
+package org.jjfflyboy.mpc4j;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author jfraney
+ */
+public class MoveIdTest {
+
+    @Test
+    public void moveWithFrom() {
+        String text = new MoveId(10, 30).text();
+        assertThat(text).isEqualTo("moveid 10 30");
+    }
+}
