8d6159117f989709082bbafd1b0c00262a097d04
John J. Franey <jjfraney@gmail.com>
2016-10-26 22:26:12 -0400

    implements clear command

---
+++ src/main/java/org/jjfflyboy/mpc4j/Clear.java
@@ -0,0 +1,9 @@
+package org.jjfflyboy.mpc4j;
+
+/**
+ * @author jfraney
+ */
+public class Clear extends Simple {
+
+    public String text() {return "clear";}
+}
---
+++ src/test/java/org/jjfflyboy/mpc4j/ClearIT.java
@@ -0,0 +1,26 @@
+package org.jjfflyboy.mpc4j;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author jfraney
+ */
+public class ClearIT {
+
+    private MPC mpc;
+    @Before
+    public void before() throws IOException {
+        mpc = new MPC();
+    }
+
+    @Test
+    public void clear() throws IOException {
+        Simple.Response r = mpc.send(new Clear());
+        assertThat(r.isOk()).isTrue();
+    }
+}
