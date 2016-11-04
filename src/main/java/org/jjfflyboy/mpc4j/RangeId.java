0a7c198a828a64b22e1a0c81e8044162557f0d52
John J. Franey <jjfraney@gmail.com>
2016-11-04 16:24:43 -0400

    permits this form of rangeid command: 'rangeid <id> :'

--- src/main/java/org/jjfflyboy/mpc4j/RangeId.java
+++ src/main/java/org/jjfflyboy/mpc4j/RangeId.java
@@ -6,7 +6,18 @@ package org.jjfflyboy.mpc4j;
  * @author jfraney
  */
 public class RangeId extends Simple {
+    /**
+     * @param id of song to play
+     * @param start of range or null
+     * @param end of range or null
+     */
     public RangeId(Integer id, Integer start, Integer end) {
         super(adapt(id), new RangeParameter(start, end));
     }
+
+    /**
+     * command form: 'rangeid <id> :'
+     * @param id of song to play
+     */
+    public RangeId(Integer id) { this(id, null, null);}
 }
