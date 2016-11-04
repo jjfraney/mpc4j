e88be2835afa5e051e40a094e1abf53463e1384b
John J. Franey <jjfraney@gmail.com>
2016-11-04 14:02:45 -0400

    'non' implementation of listall and listallinfo commands

---
+++ src/main/java/org/jjfflyboy/mpc4j/ListAllInfo.java
@@ -0,0 +1,33 @@
+package org.jjfflyboy.mpc4j;
+
+import java.util.List;
+
+/**
+ * listallinfo command from
+ * <a href='https://www.musicpd.org/doc/protocol/database.html'>MPD Document: The music database.</a>
+ * <p>
+ *     MPD documentation advises against using this command.
+ *     This command is intentionally inoperable.
+ *     This class serves as placeholder to document such.
+ * </p>
+ * @author jfraney
+ */
+public class ListAllInfo extends AbstractCommand<Command.Response> {
+
+    /**
+     * MPD documents advises against using this command.  No implementation is provided.
+     */
+    private ListAllInfo() {
+        throw new RuntimeException("Do not use.");
+    }
+
+    @Override
+    public String text() {
+        throw new RuntimeException("Do not use.");
+    }
+
+    @Override
+    public Response response(List<String> responseLines) {
+        return null;
+    }
+}
