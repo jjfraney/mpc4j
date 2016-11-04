43bb6e14b08f0fce8f71396bd1994ca40b194783
John J. Franey <jjfraney@gmail.com>
2016-11-04 15:56:42 -0400

    'non' implementation of playlist command

---
+++ src/main/java/org/jjfflyboy/mpc4j/Playlist.java
@@ -0,0 +1,33 @@
+package org.jjfflyboy.mpc4j;
+
+import java.util.List;
+
+/**
+ * playlist command from
+ * <a href='https://www.musicpd.org/doc/protocol/queue.html'>MPD Document: The current playlist.</a>
+ * <p>
+ *     MPD documentation advises against using this command.
+ *     This command is intentionally inoperable.
+ *     This class serves as placeholder to document such.
+ * </p>
+ * @author jfraney
+ */
+public class Playlist extends AbstractCommand<Command.Response> {
+
+    /**
+     * MPD documents advises against using this command.  No implementation is provided.
+     */
+    private Playlist() {
+        throw new RuntimeException("Do not use.");
+    }
+
+    @Override
+    public String text() {
+        throw new RuntimeException("Do not use.");
+    }
+
+    @Override
+    public Response response(List<String> responseLines) {
+        return null;
+    }
+}
